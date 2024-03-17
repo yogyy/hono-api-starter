36af62e3ad3f7acec5cc8760be8f019d6e4e1ee2
yogyy <yogimr261@gmail.com>
2024-03-18 05:44:30 +0700

    chore: query data select data from psql local with drizzle

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
-import { Hono } from 'hono';
+import { Hono } from "hono";
+import { showRoutes } from "hono/dev";
+import { product } from "./db/schema";
+import { Client } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { bearerAuth } from "hono/bearer-auth";
 
-const app = new Hono();
+export type Env = {
+  DATABASE_URL: string;
+  token: string;
+};
 
-app.get('/', c => {
-  return c.newResponse('test');
+const app = new Hono<{ Bindings: Env }>().basePath("/api");
+
+app.get("/env", async (c, next) => {
+  // const token = c.env.token;
+  const ip = c.req.raw.headers.get("CF-Connecting-IP");
+  return c.json({ ip });
 });
 
+app.get("/product", async (c, next) => {
+  const auth = bearerAuth({ token: c.env.token });
+  await auth(c, next);
 
-export default app
+  const client = new Client({ connectionString: c.env.DATABASE_URL });
+  const db = drizzle(client);
+  await client.connect();
+  const res = await db.select().from(product);
+
+  c.executionCtx.waitUntil(client.end());
+  return c.json(res);
+});
+
+showRoutes(app);
+export default app;
