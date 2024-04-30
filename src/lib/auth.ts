8092570c5a850b7079f345d070ab312fbe7f5d44
yogyy <yogimr261@gmail.com>
2024-04-30 23:54:38 +0700

    feat: add auth middleware

---
+++ src/lib/auth.ts
@@ -0,0 +1,11 @@
+import { bearerAuth } from "hono/bearer-auth";
+import { createFactory } from "hono/factory";
+import { Env } from ".";
+
+const factory = createFactory<{ Bindings: Env }>();
+
+export const auth = factory.createMiddleware(async (c, next) => {
+  const auth = bearerAuth({ token: c.env.token });
+
+  await auth(c, next);
+});
