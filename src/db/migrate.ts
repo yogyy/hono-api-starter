de2239f5b106d080b56499ff4b2ec700b5010bc4
yogyy <yogimr261@gmail.com>
2024-12-11 22:23:53 +0700

    chore: add logger

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,18 +2,24 @@ import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { db } from ".";
 import { config } from "dotenv";
 
-config({
-  path: ".dev.vars",
-});
+config({ path: ".dev.vars" });
 
 const main = async () => {
   try {
-    await migrate(db, {
-      migrationsFolder: "drizzle",
-    });
-    console.log(process.env.DATABASE_URL);
+    console.log("⏳ Running migrations...");
+
+    const start = Date.now();
+
+    await migrate(db, { migrationsFolder: "drizzle" });
+
+    const end = Date.now();
+
+    console.log(`✅ Migrations completed in ${end - start}ms`);
+
+    process.exit(0);
   } catch (error) {
-    console.log(error);
+    console.error("❌ Migration failed");
+    console.error(error);
     process.exit(1);
   }
 };
